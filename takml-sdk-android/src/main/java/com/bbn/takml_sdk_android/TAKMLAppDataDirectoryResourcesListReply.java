c985cdddd4669175f9406d1433fb4f26989a64da
Nate Soule <nsoule@localhost.localdomain>
2022-06-20 09:49:43 -0400

    Addings docs, example models, SDK

---
+++ takml-sdk-android/src/main/java/com/bbn/takml_sdk_android/TAKMLAppDataDirectoryResourcesListReply.java
@@ -0,0 +1,25 @@
+package com.bbn.takml_sdk_android;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Reply to query for resources in TAKML Framework's app data directory.
+ */
+public class TAKMLAppDataDirectoryResourcesListReply implements Serializable {
+    List<String> filesList;
+
+    /**
+     * Create a TAKMLAppDataDirectoryResourcesListReply to hold a list of file names.
+     */
+    public TAKMLAppDataDirectoryResourcesListReply(List<String> filesList) {
+        this.filesList = filesList;
+    }
+
+    /**
+     * Get files list from object.
+     */
+    public List<String> getFilesList() {
+        return filesList;
+    }
+}
